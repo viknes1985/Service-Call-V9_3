0c45e3b199a7985604956c962f839d31ba07ae10
viknes1985 <viknes1985@gmail.com>
2026-03-20 11:15:00 +0800

    Delete src directory

--- src/firebase.ts
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from '../firebase-applet-config.json';
-
-// Initialize Firebase SDK
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
-export const auth = getAuth(app);
